d34b72283f279c7983fbd68e64c4302e3261e7dc
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2023-04-07 18:03:52 +0530

    Fixed button

--- index2.js
+++ index2.js
@@ -4,83 +4,84 @@ const newgame = document.querySelector(".btn");
 let currentPlayer;
 let gameGrid;
 
-
 const winningPositions = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
 ];
 
-function init_game(){
+function init_game() {
     currentPlayer = "X";
-    gameGrid = ["","","","","","","","",""];
-    boxes.forEach((box,index) => {
-            box.innerHTML="";
-            boxes[index].style.pointerEvents = "all";
-            box.classList = `box box${index+1}`;
+    gameGrid = ["", "", "", "", "", "", "", "", ""];
+    boxes.forEach((box, index) => {
+        box.innerHTML = "";
+        boxes[index].style.pointerEvents = "all";
+        box.classList = `box box${index + 1}`;
     });
     newgame.classList.remove("active");
     userchance.innerHTML = `Currrent Player - ${currentPlayer}`;
 }
 init_game();
 
-function swapTurn(){
-    if(currentPlayer==="X"){
-        currentPlayer="O";
-        
-    }else{
-        currentPlayer="X"
-        
+newgame.addEventListener("click", () => {
+    init_game();
+});
+
+function swapTurn() {
+    if (currentPlayer === "X") {
+        currentPlayer = "O";
+    } else {
+        currentPlayer = "X";
     }
-    userchance.innerHTML = `Current Player ${currentPlayer}`
+    userchance.innerHTML = `Current Player ${currentPlayer}`;
 }
 
-function checkWin(){
+function checkWin() {
     let answer = "";
-    winningPositions.forEach((position) =>{
-        if((gameGrid[position[0]]!== "" || gameGrid[position[1]]!=="" || gameGrid[position[2]]!=="")&& 
-        (gameGrid[position[0]]===gameGrid[position[1]] && gameGrid[position[2]]===gameGrid[position[1]])){
-           if(gameGrid[position[0]]==="X"){
-            answer = "X";
-           } 
-           else{
+    winningPositions.forEach((position) => {
+        if (
+            (gameGrid[position[0]] !== "" ||
+                gameGrid[position[1]] !== "" ||
+                gameGrid[position[2]] !== "") &&
+            gameGrid[position[0]] === gameGrid[position[1]] &&
+            gameGrid[position[2]] === gameGrid[position[1]]
+        ) {
+            if (gameGrid[position[0]] === "X") {
+                answer = "X";
+            } else {
                 answer = "O";
-           }
-           boxes.forEach((box)=>{
-            box.style.pointerEvents="none";
-           })
-           boxes[position[0]].classList.add("win");
-           boxes[position[1]].classList.add("win");
-           boxes[position[2]].classList.add("win");
+            }
+            boxes.forEach((box) => {
+                box.style.pointerEvents = "none";
+            });
+            boxes[position[0]].classList.add("win");
+            boxes[position[1]].classList.add("win");
+            boxes[position[2]].classList.add("win");
         }
     });
-    if(answer!==""){
-        userchance.innerHTML=`Winner is -- ${answer} 🎉🎉`;
+    if (answer !== "") {
+        userchance.innerHTML = `Winner is -- ${answer} 🎉🎉`;
         newgame.classList.add("active");
         return;
-
-
     }
     let fillCount = 0;
-    gameGrid.forEach((box)=>{
-        if(box !==""){
+    gameGrid.forEach((box) => {
+        if (box !== "") {
             fillCount++;
         }
     });
-    if(fillCount===9){
-        userchance.innerHTML="Game Tied...";
+    if (fillCount === 9) {
+        userchance.innerHTML = "Game Tied...";
         newgame.classList.add("active");
     }
-    
-
 }
-function handleClick(index){
-    if(gameGrid[index]===""){
+function handleClick(index) {
+    if (gameGrid[index] === "") {
         boxes[index].innerHTML = currentPlayer;
         gameGrid[index] = currentPlayer;
         boxes[index].style.pointerEvents = "none";
@@ -88,9 +89,12 @@ function handleClick(index){
         checkWin();
     }
 }
-boxes.forEach((box,index) =>{
-    box.addEventListener("click",()=>{
+boxes.forEach((box, index) => {
+    box.addEventListener("click", () => {
         handleClick(index);
-    })
+    });
 });
-newgame.addEventListener("click",init_game());
+
+// newgame.addEventListener("hover", () => {
+//     console.log("Button Clicked");
+// });
